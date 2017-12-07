bbf1a403222091fd2ae40cc647bf8aaca85c83b0
Naoyuki MORITA <naohaq@ps.sakura.ne.jp>
2017-12-07 14:24:23 +0900

    Split matrix operations into LinearAlgebra.hs

---
+++ LinearAlgebra.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LinearAlgebra
+  ( addRowTo
+  , scaleRow
+  , eraseCols
+  , forwardErase
+  , substCol
+  , backwardSubst
+  ) where
+
+replaceAt :: Int -> a -> [a] -> [a]
+replaceAt _ _ [] = []
+replaceAt 0 y (_:xs) = y:xs
+replaceAt n y (x:xs) = x : replaceAt (n-1) y xs
+
+addRowTo :: (Num a) => Int -> Int -> a -> [[a]] -> [[a]]
+addRowTo x y w rows = replaceAt y r1' rows
+  where r0 = rows !! x
+        r1 = rows !! y
+        r1' = zipWith ((+) . (w*)) r0 r1
+
+scaleRow :: (Num a) => Int -> a -> [[a]] -> [[a]]
+scaleRow x w rows = replaceAt x r' rows
+  where r = rows !! x
+        r' = map (w*) r
+
+eraseCols :: (Num a, Fractional a, Eq a) => Int -> Int -> [[a]] -> [[a]]
+eraseCols n j mtx = foldr ers mtx' [(j+1)..(n-1)]
+  where w = (mtx !! j) !! j
+        mtx' = scaleRow j (1/w) mtx
+        ers k m | v /= 0    = addRowTo j k (negate v) m
+                | otherwise = m
+          where v = (m !! k) !! j
+
+forwardErase :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
+forwardErase n mtx = foldr (eraseCols n) mtx (reverse [0..(n-1)])
+
+substCol :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
+substCol j mtx = foldr ers mtx [0..(j-1)]
+  where ers k m | v /= 0    = addRowTo j k (negate v) m
+                | otherwise = m
+          where v = (m !! k) !! j
+
+backwardSubst :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
+backwardSubst n mtx = foldr substCol mtx [1..(n-1)]
+
+-- EOF
--- ReedSolomon.hs
+++ ReedSolomon.hs
@@ -14,6 +14,7 @@ module ReedSolomon
   ) where
 
 import qualified Polynomial as P
+import qualified LinearAlgebra as LA
 import qualified Language.Haskell.TH as TH
 import qualified TypeLevel.Number.Nat as TL
 
@@ -77,44 +78,9 @@ calcErrMatrix dec locs ss = [map (f . (j*)) locs ++ [ss !! (j-1)] | j <- [1..t]]
         t = length locs
 
 solveErrMatrix :: (Num k, Fractional k, Eq k) => [[k]] -> [k]
-solveErrMatrix mtx = map (head . drop n) $ backwardSubst n $ forwardErase n mtx
+solveErrMatrix mtx = map (head . drop n) $ LA.backwardSubst n $ LA.forwardErase n mtx
   where n = length mtx
 
-replaceAt :: Int -> a -> [a] -> [a]
-replaceAt _ _ [] = []
-replaceAt 0 y (_:xs) = y:xs
-replaceAt n y (x:xs) = x : replaceAt (n-1) y xs
-
-addRowTo :: (Num a) => Int -> Int -> a -> [[a]] -> [[a]]
-addRowTo x y w rows = replaceAt y r1' rows
-  where r0 = rows !! x
-        r1 = rows !! y
-        r1' = zipWith ((+) . (w*)) r0 r1
-
-multRow :: (Num a) => Int -> a -> [[a]] -> [[a]]
-multRow x w rows = replaceAt x r' rows
-  where r = rows !! x
-        r' = map (w*) r
-
-eraseCols :: (Num a, Fractional a, Eq a) => Int -> Int -> [[a]] -> [[a]]
-eraseCols n j mtx = foldr ers mtx' [(j+1)..(n-1)]
-  where w = (mtx !! j) !! j
-        mtx' = multRow j (1/w) mtx
-        ers k m | v /= 0    = addRowTo j k (negate v) m
-                | otherwise = m
-          where v = (m !! k) !! j
-
-forwardErase :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
-forwardErase n mtx = foldr (eraseCols n) mtx (reverse [0..(n-1)])
-
-substCol :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
-substCol j mtx = foldr ers mtx [0..(j-1)]
-  where ers k m | v /= 0    = addRowTo j k (negate v) m
-                | otherwise = m
-          where v = (m !! k) !! j
-
-backwardSubst :: (Num a, Fractional a, Eq a) => Int -> [[a]] -> [[a]]
-backwardSubst n mtx = foldr substCol mtx [1..(n-1)]
 
 calcErrors :: (Num a, Fractional a, Eq a, ReedSolomon c a) => c a -> [a] -> [(Int,a)]
 calcErrors dec synd = zip locs_r evs_r
